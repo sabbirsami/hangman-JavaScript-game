351122f0d1cd48c026c6b1d3f211dc90f3f618cc
SabbirSami <smd71430@gmail.com>
2023-08-25 18:11:58 +0600

    create word-list js file and get word

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 const keyboardDiv = document.querySelector(".keyboard");
 
+const getRandomWord = () => {
+    const { word, hint } =
+        wordList[Math.floor(Math.random() * wordList.length)];
+    console.log(word, hint);
+};
 // CREATE KEYBOARD BUTTON
 for (let i = 97; i <= 122; i++) {
     const button = document.createElement("button");
@@ -8,3 +13,4 @@ for (let i = 97; i <= 122; i++) {
 }
 
 // console.log(String.fromCodePoint(9733)); Output: "★"
+getRandomWord();
---
+++ js/word-list.js
@@ -0,0 +1,70 @@
+const wordList = [
+    {
+        word: "bicycle",
+        hint: "A human-powered vehicle with two wheels",
+    },
+    {
+        word: "sunset",
+        hint: "The daily disappearance of the sun below the horizon",
+    },
+    {
+        word: "coffee",
+        hint: "A popular caffeinated beverage made from roasted coffee beans",
+    },
+    {
+        word: "dance",
+        hint: "A rhythmic movement of the body often performed to music",
+    },
+    {
+        word: "guitar",
+        hint: "A musical instrument with strings",
+    },
+    {
+        word: "mountain",
+        hint: "A large natural elevation of the earth's surface",
+    },
+    {
+        word: "painting",
+        hint: "An art form using colors on a surface to create images or expression",
+    },
+    {
+        word: "astronomy",
+        hint: "The scientific study of celestial objects and phenomena",
+    },
+    {
+        word: "adventure",
+        hint: "An exciting or daring experience",
+    },
+    {
+        word: "diamond",
+        hint: "A precious gemstone known for its brilliance and hardness",
+    },
+    {
+        word: "camera",
+        hint: "A device used to capture and record images or videos.",
+    },
+    {
+        word: "jazz",
+        hint: "A genre of music characterize by improvisation and syncopation",
+    },
+    {
+        word: "pizza",
+        hint: "A savory dish consisting of a round, flattened base with toppings",
+    },
+    {
+        word: "history",
+        hint: "A study of past events and human civilization",
+    },
+    {
+        word: "butterfly",
+        hint: "An insect with colorful wings and a slender body",
+    },
+    {
+        word: "chocolate",
+        hint: "A sweet treat made from cocoa beans",
+    },
+    {
+        word: "football",
+        hint: "A popular sport played with a spherical ball",
+    },
+];
